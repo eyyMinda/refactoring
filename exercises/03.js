ef2a600d9678bd52c73c8747e957709b732d4498
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2023-01-27 18:01:17 +0200

    refactor 03

--- exercises/03.js
+++ exercises/03.js
@@ -1,43 +1,12 @@
 const assert = require('assert');
-
 console.clear();
 
-function fac1(list) {
-  return list;
+let multiply = 1;
+function fac(list) {
+  return list.map(skaicius => skaicius * multiply);
 }
-
-function fac2(list) {
-  return list.map(skaicius => {
-    return skaicius * 2;
-  });
-}
-
-function fac3(list) {
-  var new_grand = [];
-  let i = 0;
-  while (i < list.length) {
-    new_grand.push(list[i++] * 3);
-  }
-  return new_grand;
-}
-
 function f_a_c_t_o_r_y(skaiciu_grandinele = [], double = 1) {
-  let fac = null;
-
-  if (double == 1) {
-    fac = fac1;
-  } else if (double == 2) {
-    fac = fac2;
-  } else if (double === 3) {
-    fac = fac3;
-  } else {
-    // FIX:
-    // not sure how, but there has to be a way
-    // to call a single EXTERNAL FUCNTION with 
-    // any "double" value, so that we would not
-    // need to call infinite amount of else-if's
-  }
-
+  multiply = double;
   // DO NOT EDIT THIS LINE
   return fac(skaiciu_grandinele);
 }
